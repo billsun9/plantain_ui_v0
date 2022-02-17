6393f1171b1cf6766f0663da6d065cd120fe69a4
bill <billsun9@gmail.com>
2022-02-17 17:03:34 -0500

    api

--- src/form/inputForm.jsx
+++ src/form/inputForm.jsx
@@ -1,35 +1,78 @@
+import { useState } from 'react';
+var axios = require('axios');
+
 export const InputForm = (props) => {
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [camera, setCamera] = useState("");
+    const [microphone, setMicrophone] = useState("");
+    const [lighting, setLighting] = useState("");
+    const [keyboard, setKeyboard] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(name === '' || email === '') alert("enter name and email");
+        else {
+            var data = JSON.stringify({
+                "name": name, 
+                "email": email,
+                "camera": camera,
+                "microphone": microphone,
+                "lighting": lighting,
+                "keyboard": keyboard
+            })
+
+            var config = {
+                method: 'post',
+                url: 'http://127.0.0.1:5000/api/v0/post',
+                headers: { 
+                    'Content-Type': 'text/plain'
+                },
+                data : data
+            };
+
+            axios(config)
+                .then(function (response) {
+                console.log(JSON.stringify(response.data));
+                })
+                .catch(function (error) {
+                console.log(error);
+                });
+        }
+    }
+
     return (
         <div id="inputform">
             <div className="wrapper">
                 <h2>Registration</h2>
-                <form action="" onsubmit="submitForm(e)">
-                <div className="input-box">
-                    <input type="text" placeholder="Enter your name" required />
+                <div className='form'>
+                    <div className="input-box">
+                        <input type="text" placeholder="Enter your name" value={name} onChange={(e) => setName(e.target.value)}/>
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="Camera" value={camera} onChange={(e) => setCamera(e.target.value)} />
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="Microphone" value={microphone} onChange={(e) => setMicrophone(e.target.value)} />
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="Lighting" value={lighting} onChange={(e) => setLighting(e.target.value)} />
+                    </div>
+                    <div className="input-box">
+                        <input type="text" placeholder="Keyboard" value={keyboard} onChange={(e) => setKeyboard(e.target.value)} />
+                    </div>
+                    <div className="policy">
+                        <input type="checkbox" />
+                        <h3>I accept all <a href="./terms.html" target="_blank">terms & condition</a></h3>
+                    </div>
+                    <div className="input-box button">
+                        <input type="Submit" value="Register Now" onClick={(e) => handleSubmit(e)}/>
+                    </div>
                 </div>
-                <div className="input-box">
-                    <input type="text" placeholder="Enter your email" required />
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder="Camera" required />
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder="Microphone" required />
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder="Lighting" />
-                </div>
-                <div className="input-box">
-                    <input type="text" placeholder="Keyboard" />
-                </div>
-                <div className="policy">
-                    <input type="checkbox" />
-                    <h3>I accept all <a href="./terms.html" target="_blank">terms & condition</a></h3>
-                </div>
-                <div className="input-box button">
-                    <input type="Submit" value="Register Now" />
-                </div>
-                </form>
             </div>
         </div>
         
